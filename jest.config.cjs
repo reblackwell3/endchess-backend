92bc77e759af1aa894c0b7ccf2dcfc4e01023c2c
Robert Blackwell <reblackwell3@gmail.com>
2024-08-20 15:47:18 -0400

    style works with npm start and test

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,10 @@
-export default {
+// jest.config.js
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   testMatch: ['**/tests/**/*.test.ts'],
   moduleNameMapper: {
-    // Resolve your project's .js imports to .ts files during testing
     '^(src/.+).js$': '<rootDir>/$1.ts',
   },
   transform: {
