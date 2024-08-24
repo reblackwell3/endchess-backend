52def485b44d499a81be249b4adf62fafc0f4c90
Robert Blackwell <reblackwell3@gmail.com>
2024-08-24 19:20:46 -0400

    improve player model

--- src/features/players/playerModel.ts
+++ src/features/players/playerModel.ts
@@ -1,14 +1,28 @@
-// backend/players/playerModel.js
 import mongoose from 'mongoose';
 
 const playerSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
-  elo: { type: Number, required: true, default: 1200 },
-  puzzlesCompleted: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Puzzle' }],
-  gamesCompleted: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }],
-  importedGames: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }],
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  elo: {
+    type: Number,
+    default: 1200,
+  },
+  puzzlesCompleted: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [],
+  },
+  importedGames: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [], // Ensure the array is initialized
+  },
+  gamesCompleted: {
+    type: [mongoose.Schema.Types.ObjectId],
+    default: [], // Initialize this array too
+  },
 });
 
 const Player = mongoose.model('Player', playerSchema);
-
 export default Player;
