8a3ef9ece14a3f5906894ea260313c5b75c32052
Robert Blackwell <reblackwell3@gmail.com>
2024-09-01 17:03:25 -0400

    use port

--- src/authServer.ts
+++ src/authServer.ts
@@ -43,7 +43,7 @@ app.use(express.json());
 
 app.use('/', authRoutes);
 
-const PORT = process.env.AUTH_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
