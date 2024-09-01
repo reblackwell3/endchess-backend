ea3b598cdd9a796ee1dbe4410cada580a41859d8
Robert Blackwell <reblackwell3@gmail.com>
2024-09-01 08:55:26 -0400

    Revert "update logs in cookie strategy"
    This reverts commit c92bdc6def1d92f61385ecc2a62c232ab27704be.

--- src/features/auth/passportConfig.app.ts
+++ src/features/auth/passportConfig.app.ts
@@ -34,14 +34,10 @@ passport.use(
       console.log('Cookie Token:', token);
       try {
         const user = await User.findOne({ accessToken: token });
-        if (!user) {
-          console.log('Cookie Strategy did not find user');
-          return done(null, false);
-        }
+        if (!user) return done(null, false);
         console.log('Cookie Strategy found user: ', user);
         return done(null, user);
       } catch (error) {
-        console.log('Cookie Strategy error:', error);
         return done(error);
       }
     },
