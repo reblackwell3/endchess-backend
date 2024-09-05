b45b041b93f00b6a934331d426124bf8d8d36169
Robert Blackwell <reblackwell3@gmail.com>
2024-09-05 14:51:27 -0400

    add analyze (#42)
    * add analyze
    
    * turnover
    
    * add analyze from pgn
    
    * 8 then 18 mistake
    
    * move stockfish to docker package
    
    * dockerfile running
    
    * move tests with stockfish
    
    * correct port num
    
    * simplify analyze files
    
    * add gcc to dockerfile and correct path for stockfish tar
    
    * remove analyze and import features
    
    * remove routes

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,8 @@ import { connectDB } from './config/db';
 import puzzleRoutes from './features/puzzles/puzzleRoutes';
 import playerRoutes from './features/players/playerRoutes';
 import gameRoutes from './features/games/gameRoutes';
-import importRoutes from './features/import/importRoutes';
-import analyzeRoutes from '../docker-stockfish/src/analyze/analyzeRoutes';
-// import { attachPlayerId } from './features/_middleware/addPlayerIdMiddleware';
 import authenticateCookie from './features/auth/authenticateCookie';
-// import { createOrUpdateAuth } from './features/_middleware/createOrUpdateAuth';
+import authRoutes from './features/auth/authRoutes';
 import passport from 'passport';
 import session from 'express-session';
 import { default as connectMongoDBSession } from 'connect-mongodb-session';
@@ -46,22 +43,22 @@ app.use(
   }),
 );
 
-app.use(cookieParser(process.env.COOKIE_SECRET!)); // Use the cookie-parser middleware
+app.use(cookieParser(process.env.COOKIE_SECRET!));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(express.json());
 
+app.use('/', authRoutes);
+
 app.use(authenticateCookie);
 
 app.use('/puzzles', puzzleRoutes);
 app.use('/players', playerRoutes);
 app.use('/games', gameRoutes);
-app.use('/import', importRoutes);
-app.use('/analyze', analyzeRoutes);
 
-const PORT = process.env.APP_PORT || 5555;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
